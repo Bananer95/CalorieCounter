ead00d8558d9ff558faac4b19a6469cfb050c870
Bananer95 <Bananer95@mail.ru>
2022-05-06 22:22:02 +0200

    feat: make minimal project

--- src/js/eventBtn.js
+++ src/js/eventBtn.js
@@ -1,30 +1,64 @@
-import { eventLinks, checkBtn, checkPoitn } from './eventLinks.js';
 import * as closing from './closingEvents.js';
+export const btnReult = document.querySelector('div.btn');
 
 const ba = document.querySelector('button.btn_calculation');
 const by = document.querySelector('button.btn_clear');
-
 const popatResult = document.getElementById('result');
-export const btnReult = document.querySelector('div.btn');
+const age = document.querySelector('div.age_input>input');
+const groth = document.querySelector('div.groth_input>input');
+const eight = document.querySelector('div.eight_input>input');
+
+let normal;
+let normalPlus;
+let normalMinus;
+
+const resultNorma = document.querySelector('div.result_normal>p>span');
+const resultUp = document.querySelector('div.result_up>p>span');
+const resultDown = document.querySelector('div.result_down>p>span');
 
 function eventBtn() {
   btnReult.addEventListener('click', (event) => {
     const btnRes = event.target.closest('button.btn_calculation');
     const btnCler = event.target.closest('button.btn_clear');
+    const textAge = document.querySelector('div.link_age>h5');
+    const textGroth = document.querySelector('div.link_groth>h5');
+    const textEight = document.querySelector('div.link_eight>h5');
 
-    if (checkPoitn === 0) {
+    if (by.textContent === 'Очистить') {
       if (btnRes) {
         popatResult.classList.add('open');
+
+        normal = Math.ceil(
+          10 * Number(eight.value) +
+            6.25 * Number(groth.value) -
+            5 * Number(age.value) +
+            closing.sex
+        );
+        normalPlus = Math.ceil(normal + normal * 0.15);
+        normalMinus = Math.floor(normal - normal * 0.15);
+
+        resultNorma.textContent = normal;
+        resultUp.textContent = normalPlus;
+        resultDown.textContent = normalMinus;
       } else if (btnCler) {
         popatResult.classList.remove('open');
       }
-    } else {
+    } else if (by.textContent == 'Закрыть') {
       if (closing.age.classList.contains('show')) {
-        if (btnCler) {
-          closing.age.classList.remove('show');
-          checkPoitn = 1;
-          checkBtn();
-        }
+        closing.age.classList.remove('show');
+        by.textContent = 'Очистить';
+        ba.removeAttribute('disabled');
+        textAge.textContent = `Вашь возраст: ${age.value} `;
+      } else if (closing.groth.classList.contains('show')) {
+        closing.groth.classList.remove('show');
+        by.textContent = 'Очистить';
+        ba.removeAttribute('disabled');
+        textGroth.textContent = `Вашь рост: ${groth.value} `;
+      } else if (closing.eight.classList.contains('show')) {
+        closing.eight.classList.remove('show');
+        by.textContent = 'Очистить';
+        ba.removeAttribute('disabled');
+        textEight.textContent = `Вашь вес: ${eight.value} `;
       }
     }
   });
--- src/js/eventLinks.js
+++ src/js/eventLinks.js
@@ -1,12 +1,11 @@
 import * as closeEvent from './closingEvents.js';
-import * as btnEvent from './eventBtn.js';
 
-const btnRes = document.querySelector('button.btn_calculation');
 const btnCler = document.querySelector('button.btn_clear');
+const btnRes = document.querySelector('button.btn_calculation');
 
 // export let checkPoitn = 0;
 
-// function checkBtn() {
+// function checkBtn(checkPoitn) {
 //   if (checkPoitn === 1) {
 //     btnRes.textContent = 'Сохранить';
 //     btnCler.textContent = 'Закрыть';
@@ -20,6 +19,8 @@ const links = document.getElementById('links');
 
 function eventLinks() {
   links.addEventListener('click', (event) => {
+    btnRes.setAttribute('disabled', 'disabled');
+
     switch (event.target.dataset.action) {
       case 'link_gender': {
         closeEvent.gender.classList.add('show');
@@ -34,17 +35,20 @@ function eventLinks() {
 
       case 'link_age': {
         closeEvent.age.classList.add('show');
+        btnCler.textContent = 'Закрыть';
 
         break;
       }
 
       case 'link_groth': {
         closeEvent.groth.classList.add('show');
+        btnCler.textContent = 'Закрыть';
         break;
       }
 
       case 'link_eight': {
         closeEvent.eight.classList.add('show');
+        btnCler.textContent = 'Закрыть';
         break;
       }
     }
